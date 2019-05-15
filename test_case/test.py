c7ffefc817eab3f55dc92c264ef27e6228efbe9c
xinpeter <1@1.com>
2019-05-15 17:00:37 +0800

    挂机

--- test_case/test.py
+++ test_case/test.py
@@ -8,6 +8,9 @@ log = Log().getlog()
 while True:
     list = load_data()
     for i in list.items():
+        c_time = int(time.strftime("%H", time.localtime()))
+        while 0 < c_time < 9 or 13 < c_time < 17:
+            time.sleep(600)
         log.info("%s 开始发帖" %i[1]["username"])
         driver = webdriver.Chrome()
         driver.maximize_window()
@@ -36,6 +39,6 @@ while True:
             time.sleep(1)
         except:
             log.info("登出失败")
-        login_obj.adsl()
+        # login_obj.adsl()
         driver.quit()
         time.sleep(3600)
